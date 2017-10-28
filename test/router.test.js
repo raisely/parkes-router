653966c059ce41bcc0a26f411250d3b4b2707696
Chris Jensen <chris@broadthought.co>
2017-10-28 14:22:46 +0800

    refactor tests to use parkes-api-test

--- test/router.test.js
+++ test/router.test.js
@@ -1,91 +1,68 @@
-const ParkesRouter = require('../index');
 const pluralize = require('pluralize');
 
-const server = require("./testServer");
-const request = require("supertest");
+const server = require('./testServer');
+const describeApi = require('parkes-api-test');
 
-
-function shouldRouteCorrectly(name, actions, options) {
+function describeRoute(name, actions, options) {
 	const collectionName = pluralize(name);
 	const id = pluralize.singular(name);
+	const { parent } = options;
+
+	const prefix = parent ? `/${pluralize(parent)}/${pluralize.singular(parent)}` : '';
 
 	const routes = {
-		index: { method: 'get', path: `/${collectionName}` },
-		show: { method: 'get', path: `/${collectionName}/${id}` },
-		create: { method: 'post', path: `/${collectionName}/` },
-		update: { method: 'patch', path: `/${collectionName}${id}` },
-		remove: { method: 'delete', path: `/${collectionName}` },
+		index: { method: 'get', path: `${prefix}/${collectionName}` },
+		show: { method: 'get', path: `${prefix}/${collectionName}/${id}` },
+		create: { method: 'post', path: `${prefix}/${collectionName}/` },
+		update: { method: 'patch', path: `${prefix}/${collectionName}/${id}` },
+		remove: { method: 'delete', path: `${prefix}/${collectionName}` },
 	};
+	const testRoutes = [];
 
-	routes.forEach((action) => {
-		const route = routes[action];
-		if (!route) throw new Error(`Unknown action ${action}`);
-
-		describe(action, () => {
-			beforeEach(() => {
-
-			});
-			const response = await req[route.method](route.path);
-
-			expect(response.status).toEqual(200);
-			expect(response.body.data.path).toEqual(route.path);
-			expect(response.body.data.name).toEqual(route.name);
-			expect(response.body.data.action).toEqual(action);
-		})
+	// Validate list of actions
+	actions.forEach((action) => {
+		if (!routes[action]) throw new Error(`Unknown action ${action}`);
 	});
+
+	Object.keys(routes).forEach((action) => {
+		const route = routes[action];
+
+		// Ensure correct routing
+		if (actions.includes(action)) {
+			if (action === 'index') {
+				route.expect = {
+					name,
+					path: route.path,
+					action: route.action,
+					presentationMethod: 'toPublic',
+				};
+			}
+
+			if (['index', 'show'].includes(action)) {
+				testRoutes.push(privateAction(route));
+			}
+		} else {
+			// Otherwise the route shouldn't be defined
+			route.status = 404;
+		}
+
+		testRoutes.push(route);
+	});
+
+	describeApi(options.name || name, server, testRoutes);
 }
 
 describe('ParkesRouter', () => {
-	describe('isPrivate', () => {
-		context('when private=1', () => {
-			it('is true', () => {
-				const ctx = { query: { private: 1 } };
-				const result = ParkesRouter.isPrivate(ctx);
-				expect(result).to.be.true();
-			});
-		});
-		context('when private=false', () => {
-			it('is false', () => {
-				const ctx = { query: { private: false } };
-				const result = ParkesRouter.isPrivate(ctx);
-				expect(result).to.be.false();
-			});
-		});
-	});
-
 	describe('resource', () => {
-		let req;
-
-		beforeEach(() => {
-			req = request(server);
-		});
-
-		afterEach(() => {
-			server.close();
-		})
-
-		context('child', () => {
-			const routes = ['index', 'create'];
-
-			routes.forEach((route) => {
-				shouldRouteCorrectly(route);
-			});
-			// it defines findAll and create
-			// routes to public presenter
-		});
-		context('parent', () => {
-
-		});
-		context('limited actions', () => {
-			const routes = ['index', 'show'];
-
-			routes.forEach((route) => {
-				shouldRouteCorrectly(route);
-			});
-
-			it('should not route to create', () => {
-
-			});
-		});
+		describeRoute('parent');
+		describeRoute('child', ['index', 'create'], { parent: 'parent' });
+		describeRoute('sibling', ['index', 'show'], { name: 'limited actions' });
 	});
 });
+
+function privateAction(route) {
+	const privateRoute = Object.assign({}, route);
+	privateRoute.path += '?private=true';
+	privateRoute.presentationMethod = 'toPrivate';
+	return privateRoute;
+}
